fc814c6240ced6463e3ae3bb92206391b554f1b7
Soubhagya Gautam <soubhagyag25@gmail.com>
2024-08-25 19:00:22 +0530

    ADDED the test for SIGN UP

--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -1,19 +1,30 @@
 import { expect } from 'chai';
 import request from 'supertest';
 
+
 import app from '../../src/index';
 
-describe('User APIs Test', () => {
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app.getApp())
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
-
-          done();
-        });
+describe('Fundoo Notes Integration testing', () => {
+  describe('User APIs Test', () => {
+    let userDetails: { firstName: string; lastName: string; password: string; email: string }
+      = {
+      "firstName": "bheem12",
+      "lastName": "kumar",
+      "password": "bheem@123",
+      "email": "bheem@gmail.com"
+    }
+    describe('Register A User', () => {
+      it('Registration Of User', (done) => {
+        request(app.getApp())
+          .post('/api/v1/users/signup')
+          .send(userDetails)
+          .end((err, res) => {
+            console.log(res.body);
+            expect(res.statusCode).to.be.equal(201);
+            done();
+          });
+      });
     });
-  });
-});
+
+  })
+})
